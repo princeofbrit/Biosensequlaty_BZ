4980758092f242ba7f369765e784afa5d426dde4
Bo Zhang <b021z055@kumc.edu>
2020-08-07 12:57:57 -0500

    patch1
    new adds

--- R/repeated_facility_table_excel.R
+++ R/repeated_facility_table_excel.R
@@ -1,4 +1,10 @@
-write_facility_bo_excel <- function(input){
+#' @description
+#' This function uses excel file as input to produce
+#' @import dplyr
+#' @import tidyr
+#' @import readxl
+
+repeated_facility_bo_excel <- function(input, contact){
   Input <- read_excel(input, col_names = FALSE)
   
   username <- as.character(Input[1,2])
@@ -7,24 +13,23 @@ write_facility_bo_excel <- function(input){
   mft <- as.character(Input[4,2])
   start <- as.POSIXct(as.numeric(Input[5,2] ) *(60*60*24),origin= '1899-12-30')  
   end <- as.POSIXct(as.numeric(Input[6,2] ) *(60*60*24),origin= '1899-12-30')  
-  facility <- as.character(Input[7,2])
+
   directory <- as.character(Input[8,2])
   field <- ifelse(is.na(Input[9,2]),NA, as.character(Input[9,2]))
   exclude <- ifelse(is.na(Input[10,2]),NA, as.character(Input[10,2]))
   email <-as.logical (Input[11,2])
   sender<-as.character(Input[12,2])
   email_password <-as.character(Input[13,2])
-  receiver <-as.character(Input[14,2])
+  
   personname<-as.character(Input[15,2])
   title<- as.character(Input[16,2])
   phone<- as.character(Input[17,2])
-    
-  write_facility_report(username=username, password=password, 
+  
+  repeated_facility_table(contact=contact, username=username, password=password, 
                         table=table, mft=mft,
                         start=start, 
                         end=end,
-                        facility=facility,
                         directory=directory,field=field,exclude=exclude,
-                        email =email, sender=sender,receiver=receiver,
+                        email =email, sender=sender,
                         email_password=email_password,personname=personname,title=title, phone=phone)
 }
